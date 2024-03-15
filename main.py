3128e652394b307db16d36473f39bd28a1305398
juankarlov <163530734+juankarlov@users.noreply.github.com>
2024-03-15 21:14:43 +0800

    main.py

--- main.py
+++ main.py
@@ -19,3 +19,12 @@ plt.grid(True)
 plt.xticks(media_df['Year']) 
 plt.show()
 
+df = pd.read_csv('social media use by age.csv')
+percentage = df['Percentage'].str.rstrip('%').astype('float')
+age_groups = df['Age Group'].tolist()
+
+plt.figure(figsize=(8, 8))
+plt.pie(percentage, labels=age_groups, autopct='%1.1f%%', startangle=0)
+plt.axis('equal')
+plt.title('Social Media Use by Age Group in the US in 2023')
+plt.show()
